7a1e7d0c69ab7991531942f5ba176d4387aa8279
Carlos Wagner <carlos.sperling@granter.com.br>
2021-06-25 18:41:12 -0300

    Adicionado camada de aplicação.

--- ProjetoModeloDDD.Application/Properties/AssemblyInfo.cs
+++ ProjetoModeloDDD.Application/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e7dc03a4-c9b3-447a-a93b-16a5e91a69ac")]
+[assembly: Guid("e302b916-7b09-4628-87c7-3402e1674ef2")]
 
 // Version information for an assembly consists of the following four values:
 //
