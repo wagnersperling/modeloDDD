27298b3878ff91850a3351f22b891d79719487d7
Carlos Wagner <carlos.sperling@granter.com.br>
2021-06-25 18:37:49 -0300

    Criado serviços na camada de domínio.

---
+++ ProjetoModeloDDD.Domain/Services/ClienteService.cs
@@ -0,0 +1,27 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces.Repositories;
+using ProjetoModeloDDD.Domain.Interfaces.Services;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoModeloDDD.Domain.Services
+{
+    public class ClienteService : ServiceBase<Cliente>, IClienteService
+    {
+        private readonly IClienteRepository _clienteRepository;
+        public ClienteService(IClienteRepository clienteRepository) 
+            : base(clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        public IEnumerable<Cliente> ObterClientesEspeciais(IEnumerable<Cliente> clietes)
+        {
+            var listaClientes = clietes.Where(c => c.ClienteEspecial(c));
+            return listaClientes;
+            //2:10
+        }
+    }
+}
+
+
