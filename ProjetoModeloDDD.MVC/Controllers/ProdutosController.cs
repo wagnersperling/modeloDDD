1fdc345bb3033651e9e2bb841689dfa38123e18b
Carlos Wagner <carlos.sperling@granter.com.br>
2021-06-27 08:59:28 -0300

    Adicionado telas de produto.

---
+++ ProjetoModeloDDD.MVC/Controllers/ProdutosController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using ProjetoModeloDDD.Application.Interface;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.MVC.viewModels;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ProjetoModeloDDD.MVC.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly IProdutoAppService _produtoAppService;
+        private readonly IClienteAppService _clienteAppService;
+
+        public ProdutosController(IProdutoAppService produtoAppService, IClienteAppService clienteAppService)
+        {
+            _produtoAppService = produtoAppService;
+            _clienteAppService = clienteAppService;
+        }
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            var produtos = _produtoAppService.GetAll();
+            var produtosViewModel = Mapper.Map<IEnumerable<Produto>, IEnumerable<ProdutoViewModel>>(produtos);
+            return View(produtosViewModel);
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(int id)
+        {
+            var produtoViewModel = Mapper.Map<Produto, ProdutoViewModel>(_produtoAppService.GetById(id));
+            return View(produtoViewModel);
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClienteId = new SelectList(_clienteAppService.GetAll(), "ClienteId", "Nome");
+            return View();
+        }
+
+        // POST: Produtos/Create
+        [HttpPost]
+        public ActionResult Create(ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var produto = Mapper.Map<ProdutoViewModel, Produto>(produtoViewModel);
+                _produtoAppService.Add(produto);
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClienteId = new SelectList(_clienteAppService.GetAll(), "ClienteId", "Nome");
+            return View(produtoViewModel);            
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(int id)
+        {            
+            var produtoViewModel = Mapper.Map<Produto, ProdutoViewModel>(_produtoAppService.GetById(id));
+            ViewBag.ClienteId = new SelectList(_clienteAppService.GetAll(), "ClienteId", "Nome", null, produtoViewModel.Cliente.ClienteId );
+            return View(produtoViewModel);
+        }
+
+        // POST: Produtos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var produto = Mapper.Map<ProdutoViewModel, Produto>(produtoViewModel);
+                _produtoAppService.Update(produto);
+                return RedirectToAction("Index");
+            }
+            return View(produtoViewModel);
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var produto = _produtoAppService.GetById(id);
+            var produtoViewModel = Mapper.Map<Produto, ProdutoViewModel>(produto);
+            return View(produtoViewModel);
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var produto = _produtoAppService.GetById(id);
+            _produtoAppService.Remove(produto);
+            return RedirectToAction("Index");
+        }
+    }
+}
